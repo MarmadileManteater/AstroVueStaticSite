82f02365d9a4bb4ea64a8a5ec4d7cd623ad57cba
Emma <MarmadileManteater@proton.me>
2022-11-09 22:28:59 -0500

    Revert change
    After it did not do what I wanted

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()],
-  base: './'
+  integrations: [vue(), tailwind()]
 });
