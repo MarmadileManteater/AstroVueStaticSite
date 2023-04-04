ce443fac47de0f4c84d309fb22fd884acc1d2da9
Emma <MarmadileManteater@proton.me>
2023-04-03 20:08:22 -0400

    Fix z-index of link in godot-scene-background

--- src/components/godot-scene-background/godot-scene-background.vue
+++ src/components/godot-scene-background/godot-scene-background.vue
@@ -15,7 +15,7 @@ export default defineComponent({
 </script>
 
 <template>
-    <span class='z-10 top-2 left-0 md:absolute'>
+    <span class='z-10 relative top-2 left-0 md:absolute'>
       <a href='https://github.com/MarmadileManteater/JustThreeBlocksBackground' target='_blank' class='pt-2 pl-2 hover:underline text-white'>Background</a> <a class='pt-2 hover:underline text-white' href='https://godotengine.org/'>made with <EmojiImage emoji='🤖' />Godot</a>
     </span>
     <iframe 
